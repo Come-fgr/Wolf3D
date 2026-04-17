2d1bed97b6ff0319ca0f2465e7d6b887a581adcd
Valentin DUNON <valentin.dunon@epitech.eu>
2026-04-17 17:12:50 +0200

    CI: repaired check_allowed_functions and now handling regex

--- .github/workflows/scripts/check_allowed_functions.sh
+++ .github/workflows/scripts/check_allowed_functions.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+## Error handling
 if [[ ! "$1" || "$1" = "-h" || ! "$2" ]]; then
-    >&2 echo "check_allowed_functions: expected format: ./check_allowed_functions.sh [allowed1,allowed2,...] [BINARIES] (check_statics)"
-    >&2 echo "  Returns 1 if there is a fordbidden function or a function that should be a static one"
+    >&2 echo "format: ./check_allowed_functions.sh [alloweds,...] [BINARIES] (check_statics)"
+    >&2 echo "      Can add LIBC to allowed functions"
+    >&2 echo "      Returns 1 if there is a fordbidden function or a function that should be a static one"
     exit 1
 fi
 
@@ -10,17 +12,37 @@ fi
 alloweds="$1"
 bins="$2"
 static_required="$3"
+SEPARATOR=","
 
 ## Authorized binaries array
-bins_arr=(${bins//","/ })
+alloweds_arr=(${alloweds//$SEPARATOR/ })
+bins_arr=(${bins//$SEPARATOR/ })
 
 ## All headers files with prototypes
 all_defs=$(find ./include -iname "*.h" -type f -exec cat {} +)
 
 ## An error occured
-throw_error=false
+throw_error=fals
+
+print_authorized_function() {
+    echo "[✅] - Function $1 is authorized or not external"
+}
+
+name_match_reg() {
+    for allowed in "${alloweds_arr[@]}"; do
+        if [[ $1 =~ ^$allowed ]]; then
+            echo "true"
+            return
+        fi
+    done
+    echo "false"
+}
 
 should_be_static() {
+    if [[ $(name_match_reg $f) == "true" ]]; then
+        print_authorized_function $f
+        return
+    fi
     if [[ $static_required = "false" ]]; then
         echo "::warning title=Function should be static '$f'::$f - Not defined inside a header"
         return
@@ -30,32 +52,35 @@ should_be_static() {
 }
 
 function_not_found_in_headers() {
-    if [[ $is_extern == $f ]]; then
+    if [[ $in_libc == $f ]]; then
         should_be_static $static_required
-    elif [[ $alloweds != "ALL" ]]; then
+    elif [[ ! $alloweds =~ "LIBC" ]]; then
         echo "::error title=Function not allowed '$f'::$f - Not allowed - Allowed functions are $alloweds"
         throw_error=true
+    else
+        print_authorized_function $f
     fi
 }
 
 check_if_function_is_authorized() {
+    functions=$(nm -P $bin -g | awk '$1 !~ /^_/ && $1 != "main" && $1 != "data_start" {print $1}')
+    functions=(${functions})
+
     for fn in "${functions[@]}"; do
         f=$(echo "$fn" | cut -d "@" -f 1)
-        is_extern=$(echo "$fn" | cut -d "@" -f 2)
-        if [[ ! $is_extern && $f =~ "sf*" ]]; then continue; fi
+        in_libc=$(echo "$fn" | cut -d "@" -f 2)
+        #if [[ ! $in_libc && $f =~ "sf*" ]]; then continue; fi
         def=$(echo "$all_defs" | grep "$f(" || true)
         if [[ "$def" = "" && ! $(echo "$alloweds" | grep "$f") ]]; then
             function_not_found_in_headers
         else
-            echo "[✅] - Function $f is authorized or not external"
+            print_authorized_function $f
         fi
     done
 }
 
 for bin in "${bins_arr[@]}"; do
     echo "Checking inside $bin binary"
-    functions=$(nm -P $bin -g | awk '$1 !~ /^_/ && $1 != "main" && $1 != "data_start" {print $1}')
-    functions=(${functions})
     check_if_function_is_authorized
 done
 
