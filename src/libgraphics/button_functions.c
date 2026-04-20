9aeb29239a626106e0c685701fc8e5ecaa9e9bee
Valentin DUNON <valentin.dunon@epitech.eu>
2026-04-20 21:42:52 +0200

    feat: add error handling in button_functions.c

--- src/libgraphics/button_functions.c
+++ src/libgraphics/button_functions.c
@@ -15,10 +15,14 @@
 
 void b_start(game_t *game)
 {
+    if (game == NULL)
+        return;
     game->cur_scene = GAME;
 }
 
 void b_quit(game_t *game)
 {
+    if (game == NULL)
+        return;
     sfRenderWindow_close(game->window);
 }
