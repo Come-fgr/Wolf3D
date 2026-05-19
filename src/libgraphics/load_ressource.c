8d730210030d45e6a569e19a214f044f9d48b1ad
come_fgr <come.fougere@epitech.eu>
2026-05-19 17:32:57 +0200

    refactor: move init_ressource_list in load_ressource.c

--- src/libgraphics/load_ressource.c
+++ src/libgraphics/load_ressource.c
@@ -7,7 +7,9 @@
 
 #include <dirent.h>
 #include <stdlib.h>
+#include <SFML/Graphics/Sprite.h>
 #include <SFML/Graphics/Texture.h>
+#include <SFML/Graphics/Font.h>
 #include "macro.h"
 #include "my.h"
 #include "struct/ressource.h"
@@ -21,7 +23,7 @@ static int clean(char *name, sfTexture *texture)
     return ERROR;
 }
 
-int load_ressource(ressource_bis_t *ressources)
+static int load_ressource(ressource_bis_t *ressources)
 {
     DIR *dir = opendir(SPRITE_DIR);
     struct dirent *entry = readdir(dir);
@@ -37,5 +39,50 @@ int load_ressource(ressource_bis_t *ressources)
         entry = readdir(dir);
     }
     closedir(dir);
+    return SUCCESS;
+}
+
+static int init_sprite(component_ressource_t *ressource, char *texture_path)
+{
+    ressource->texture = sfTexture_createFromFile(texture_path, NULL);
+    if (!ressource->texture)
+        return ERROR;
+    ressource->sprite = sfSprite_create();
+    if (!ressource->sprite)
+        return ERROR;
+    sfSprite_setTexture(ressource->sprite, ressource->texture,
+        sfTrue);
+    return SUCCESS;
+}
+
+static int init_font(component_ressource_t *ressource, char *font_path)
+{
+    ressource->font = sfFont_createFromFile(font_path);
+    if (ressource->font == NULL)
+        return ERROR;
+    return SUCCESS;
+}
+
+//TODO: Use load_ressource + add MUSIC
+int init_ressource_list(component_ressource_t *ressource_list)
+{
+    int exit = SUCCESS;
+
+    for (ressource_id_t id = 0; id < NB_RESSOURCE; id++) {
+        ressource_list[id].id = id;
+        ressource_list[id].name = my_strdup(RESSOURCE_LIST[id].name);
+        switch (RESSOURCE_LIST[id].type) {
+            case TEXTURE:
+                exit = init_sprite(&ressource_list[id],
+                    RESSOURCE_LIST[id].ressource_path);
+                break;
+            case FONT:
+                exit = init_font(&ressource_list[id],
+                    RESSOURCE_LIST[id].ressource_path);
+                break;
+        }
+        if (exit == ERROR)
+            return ERROR;
+    }
     return SUCCESS;
 }
