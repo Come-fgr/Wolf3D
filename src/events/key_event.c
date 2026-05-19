d4f8afcdff1d9e2d721e3f2cf558e18501fc7df0
Valentin DUNON <valentin.dunon@epitech.eu>
2026-05-19 18:31:26 +0200

    feat: handling multiple keys for the same action

--- src/events/key_event.c
+++ src/events/key_event.c
@@ -12,25 +12,48 @@
 #include "events/keys.h"
 #include "struct/game.h"
 
+static bool is_a_key_pressed(const sfKeyCode arr[])
+{
+    for (size_t i = 0; arr && arr[i] != sfKeyUnknown; i++)
+        if (sfKeyboard_isKeyPressed(arr[i]))
+            return true;
+    return false;
+}
+
+static bool key_in_arr(sfKeyCode key, const sfKeyCode arr[])
+{
+    for (size_t i = 0; arr && arr[i] != sfKeyUnknown; i++)
+        if (arr[i] == key)
+            return true;
+    return false;
+}
+
+static void key_manage_key_row(
+    sfEvent *evt, game_t *game, key_flag_t flags, const key_event_t arr[])
+{
+    if (!key_in_arr(evt->key.code, arr->keys)
+        || (flags & KEY_ALL_REQUIRED && is_a_key_pressed(arr->keys)))
+        return;
+    if (arr->fnct)
+        arr->fnct(game);
+}
+
 static void key_manage(
-    sfEvent *evt, game_t *game, _Bool add_to_keys, const key_event_t arr[])
+    sfEvent *evt, game_t *game, key_flag_t flags, const key_event_t arr[])
 {
     if (evt == NULL || game == NULL || arr == NULL)
         return;
-    game->keys[evt->key.code] = add_to_keys;
-    for (int i = 0; arr && arr[i].key != sfKeyUnknown; i++) {
-        if (evt->key.code == arr[i].key && arr[i].fnct) {
-            arr[i].fnct(game);
-        }
-    }
+    game->keys[evt->key.code] = flags & KEY_ADD;
+    for (size_t i = 0; arr && arr[i].keys[0] != sfKeyUnknown; i++)
+        key_manage_key_row(evt, game, flags, &arr[i]);
 }
 
 void key_pressed(sfEvent *evt, game_t *game)
 {
-    return key_manage(evt, game, true, key_pressed_evts);
+    return key_manage(evt, game, KEY_ADD, key_pressed_evts);
 }
 
 void key_released(sfEvent *evt, game_t *game)
 {
-    return key_manage(evt, game, false, key_released_evts);
+    return key_manage(evt, game, KEY_ALL_REQUIRED, key_released_evts);
 }
