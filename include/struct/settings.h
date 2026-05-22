26bacbda60ee767177c8efea572d1581b7525428
come_fgr <come.fougere@epitech.eu>
2026-05-22 04:09:39 +0200

    fix: set boundaries to settings values

--- include/struct/settings.h
+++ include/struct/settings.h
@@ -11,6 +11,13 @@
     #include <stdbool.h>
     #include <SFML/Graphics/RenderWindow.h>
 
+    #define MAX_VOLUME 100
+    #define DEFAULT_VOLUME 50
+    #define MIN_VOLUME 0
+    #define MAX_FOV 100
+    #define DEFAULT_FOV (M_PI / 3.0f)
+    #define MIN_FOV 0
+
 typedef struct settings_s {
     float music_volume;
     bool fullscreen;
