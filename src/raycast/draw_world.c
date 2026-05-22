35c16f4e030280bc066777e8210547ea3a32d4db
Uriel <uriel.beugre-lupetu-kamba@epitech.eu>
2026-05-22 10:00:48 +0200

    feat: refactor code for world draw

--- src/raycast/draw_world.c
+++ src/raycast/draw_world.c
@@ -8,19 +8,33 @@
 #include "graphics.h"
 #include "castray.h"
 
-void draw_world(game_t *game)
+static void draw_sky(sfRenderWindow *win)
 {
-    sfRectangleShape *rect = NULL;
-    sfRenderWindow *win = game->window;
+    sfRectangleShape *rect = sfRectangleShape_create();
 
-    rect = sfRectangleShape_create();
-    sfRectangleShape_setSize(rect, (sfVector2f){WIN_WIDTH, WIN_HEIGHT / 2.0f});
+    sfRectangleShape_setSize(rect,
+        (sfVector2f){WINDOW_WIDTH, WINDOW_HEIGHT / 2.0f});
     sfRectangleShape_setPosition(rect, (sfVector2f){0, 0});
     sfRectangleShape_setFillColor(rect, (sfColor){80, 80, 120, 255});
     sfRenderWindow_drawRectangleShape(win, rect, NULL);
-    sfRectangleShape_setPosition(rect, (sfVector2f){0, WIN_HEIGHT / 2.0f});
+    sfRectangleShape_destroy(rect);
+}
+
+static void draw_ground(sfRenderWindow *win)
+{
+    sfRectangleShape *rect = sfRectangleShape_create();
+
+    sfRectangleShape_setSize(rect,
+        (sfVector2f){WINDOW_WIDTH, WINDOW_HEIGHT / 2.0f});
+    sfRectangleShape_setPosition(rect, (sfVector2f){0, WINDOW_HEIGHT / 2.0f});
     sfRectangleShape_setFillColor(rect, (sfColor){60, 140, 60, 255});
     sfRenderWindow_drawRectangleShape(win, rect, NULL);
     sfRectangleShape_destroy(rect);
-    draw_walls(win, &game->plr);
+}
+
+void draw_world(game_t *game)
+{
+    draw_sky(game->window);
+    draw_ground(game->window);
+    draw_walls(game);
 }
