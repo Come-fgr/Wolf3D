093e2122069f681a594312bd1ffe67732c100195
come_fgr <come.fougere@epitech.eu>
2026-05-22 10:19:02 +0200

    refactor: delete unused function in my.h

--- include/my.h
+++ include/my.h
@@ -13,18 +13,6 @@
 
     #define ABS(value) value < 0 ? -value : value
 
-size_t minidprintf(int std, const char *format, ...);
-size_t my_putfloat_base(int std, double nbr, char const *base, size_t digits);
-size_t my_putnbr_base(int std, long long nbr, char const *base);
-size_t my_putstr(int std, char const *str);
-size_t my_putchar(int std, char c);
-long long my_pow(int nb, int p);
-double my_round(double nbr, size_t digits);
-size_t my_strlen(char const *str);
-int my_strcmp(char const *s1, char const *s2);
-int my_strncmp(char const *s1, char const *s2, int n);
-long my_strtol(char const *str, char **restrict endptr);
-char *my_strdup(char const *src);
 void print_array(char **array);
 void free_array(char **array);
 bool check_array_malloc(void **array, size_t len_array);
